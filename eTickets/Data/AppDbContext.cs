4f70edb91bf06c555c45227d9e0b9cd8be77d66c
DanLi14 <daniel1490.li@gmail.com>
2022-05-11 17:11:23 +0100

    Added various models and set-up DBContext file.

---
+++ eTickets/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace eTickets.Data
+{
+    // Had to install DbContext from Microsoft.EntityFrameworkCore
+    // by using suggested actions
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
